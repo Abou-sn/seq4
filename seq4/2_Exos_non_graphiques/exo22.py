34b54b56a9ddca2beb7960f7b65ba06e09f3cb25
Abou-sn <seckaboubacar1301@gmail.com>
2025-12-01 21:10:57 +0100

    exo prefixe Gemini

--- seq4/2_Exos_non_graphiques/exo22.py
+++ seq4/2_Exos_non_graphiques/exo22.py
@@ -3,17 +3,22 @@ def est_sous_chaine(s1,s2):
         return True
     if s2 == "" :
         return False
-    if s1[0]== s2[0] :
+    if s1[0] == s2[0] :
         return est_sous_chaine(s1[1:], s2[1:])
     else :
         return est_sous_chaine(s1, s2[1:])
     
-
-s1 = 'Abou'
-s2 = 'Aboubacar SECK'
-
-print(est_sous_chaine(s1,s2))
-
-t1 = 'ASECK'
-
-print(est_sous_chaine(t1,s2))
+def prefixe_commun(s1,s2):
+    if s1 == "" or s2 =="" :
+        return ""
+    if s1[0] != s2[0] :
+        return ""
+    if s1 == s2 :
+        return s1
+    if s1[0] == s2[0] :
+        return s1[0] + prefixe_commun(s1[1:],s2[1:])
+    
+assert prefixe_commun("guitare", "guimauve") == "gui"
+assert prefixe_commun("cartes", "carbone") == "car"
+assert prefixe_commun("python", "java") == ""
+assert prefixe_commun("test", "test") == "test"
