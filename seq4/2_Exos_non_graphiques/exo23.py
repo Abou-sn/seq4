e9782857ab1c2553fbaabf8ed2e014602a8257f1
Abou-sn <seckaboubacar1301@gmail.com>
2025-12-02 22:43:30 +0100

    23

--- seq4/2_Exos_non_graphiques/exo23.py
+++ seq4/2_Exos_non_graphiques/exo23.py
@@ -11,4 +11,35 @@ def compte_str(arb) :
             S += compte_str(e)
         return S
         
-print(compte_str(arborescence))
+def afficher_str (arb): 
+    if isinstance(arb,str) :
+        print(arb, end=' ')
+    else :
+        for e in arb: 
+            afficher_str(e)
+        
+
+def compression(tab):
+    l = []
+    cpt = 0
+    nbCourant = tab[0]
+    for i in range (len(tab)):
+        if nbCourant == tab[i]:
+            cpt+=1
+        else :
+            l.append(cpt)
+            l.append(nbCourant)
+            cpt = 0
+            nbCourant = tab[i]
+
+    return l
+
+tab = [5,5,5,5,5,5,5]
+
+def cpt_points(s,i):
+    if i >= len(s):
+        return 0
+    elif s[i] == '.':
+        return 1 + cpt_points(s,i+1)
+    
+print(cpt_points("...",0))
